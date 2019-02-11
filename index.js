9bdc7b98fd72717f02a40c895599f9bc5bc5800e
Willden <P2859984@CORP.CHARTERCOM.COM>
2019-02-11 16:26:19 -0500

    Initial Commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const login = require("facebook-chat-api");
+const Scry = require("scryfall-sdk");
+
+login({"email": process.env.CG_USERNAME, "password": process.env.CG_PASSWORD}, (err, api) => {
+    if(err) return console.error(err);
+
+    api.listen((err, message) => {
+        if (message.body.match("!card")) {
+
+            Scry.Cards.byName(message.body.slice(6)).then(
+                result => api.sendMessage(result.name + " | " + result.mana_cost + "\n" +
+                result.type_line + '\n' +
+                result.oracle_text +
+                (result.power ? '\n' + result.power + ' / ' + result.toughness: ""), message.threadID))
+        }
+
+        else if (message.body.match("!price")) {
+            Scry.Cards.byName(message.body.slice(7)).then(
+                result => api.sendMessage(result.name + " | " + result.usd, message.threadID)
+            )
+        }
+    });
+});
